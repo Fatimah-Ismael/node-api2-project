f836d09f28503417c3673aa360928a82ae814484
Fatimah Ismael <fatimahismael@yahoo.com>
2023-03-27 12:04:28 -0700

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,9 +1,10 @@
 // implement your posts router here
 const express = require('express')
-const Posts = require('./posts-model')
+//const Posts = require('./posts-model')
 
-const postRouter = express.Router()
+const router = express.Router()
 
+/*
 postRouter.get('/', (req, res)=>{
     Posts.find()
     .then(posts => {
@@ -13,4 +14,23 @@ postRouter.get('/', (req, res)=>{
         console.log(error);
         res.status(500).json({message: "The posts information could not be retrieved"})
     })
+}) */
+router.get('/' , (req, res)=>{
+ res.json('yay')
 })
+router.get( '/:id', (req, res)=>{
+
+})
+router.post('/' , (req, res)=>{
+
+})
+router.delete('/:id' , (req, res)=>{
+
+})
+router.put('/:id' , (req, res)=>{
+
+})
+router.get('/' , (req, res)=>{
+
+})
+ module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,14 +1,14 @@
 // implement your server here
 // require your posts router and connect it here
 const express = require('express')
-const postRouter = require('./posts/posts-router')
+const postsRouter = require('./posts/posts-router')
 
 const server= express();
 
 server.use(express.json());
-server.use('/api/posts', postRouter)
+server.use('/api/posts', postsRouter)
 
-const Posts = require('./posts-model')
+//const Posts = require('./posts-model')
 server.use('/', (req, res)=>{
     res.status(404).json({
         message: 'not found'

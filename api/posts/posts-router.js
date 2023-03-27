3c6bdd69553dae9d80fdb8e8627a937415d8f152
Fatimah Ismael <fatimahismael@yahoo.com>
2023-03-27 11:46:10 -0700

    work in progress

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,16 @@
 // implement your posts router here
+const express = require('express')
+const Posts = require('./posts-model')
+
+const postRouter = express.Router()
+
+postRouter.get('/', (req, res)=>{
+    Posts.find()
+    .then(posts => {
+        res.status(200).json([posts])
+    })
+    .catch(error=> {
+        console.log(error);
+        res.status(500).json({message: "The posts information could not be retrieved"})
+    })
+})
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it here
+const server= require('./api/server');
+
+//const port = 5000
+server.listen(3000, ()=>{
+  console.log('server running on port 3000')
+})
